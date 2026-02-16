1279785ba6f305444dabc7844e243b052940708d
sabina213 <sabinasubedi213.com>
2026-02-16 23:42:44 +0545

    about

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $username = 'root';
 $password = '' ;
 $database = 'staymate';
-$port = 3307;
+$port = 3306;
 $conn = mysqli_connect($host, $username,$password, $database, $port);
 
 if($conn->connect_error){
